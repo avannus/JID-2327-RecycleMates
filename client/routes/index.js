11ac9440b0243c7c291b02e073f7147384364b78
jenny <jliu866@gatech.edu>
2023-02-26 23:00:17 -0500

    edit ActivePickupsDriver page name to resolve merge conflicts

--- client/routes/index.js
+++ client/routes/index.js
@@ -11,7 +11,7 @@ import Edit from './Edit';
 import DriverHome from './DriverHome';
 import ConfirmEmail from './ConfirmEmail';
 import ForgotPass from './ForgotPass';
-import ActivePickups from './Active Pickups - Driver';
+import ActivePickupsDriver from './ActivePickupsDriver';
 import AvailableRoutes from './Driver - Available Routes';
 import DescribeMaterial from './DescribeMaterial';
 
@@ -32,7 +32,7 @@ function RMRoutes() {
         <Stack.Screen name='ConfirmEmail' component={ConfirmEmail} />
         <Stack.Screen name='ForgotPass' component={ForgotPass} />
         <Stack.Screen name='AvailableRoutes' component={AvailableRoutes} />
-        <Stack.Screen name='Active Pickups' component={ActivePickups} />
+        <Stack.Screen name='ActivePickupsDriver' component={ActivePickupsDriver} />
         <Stack.Screen name='DescribeMaterial' component={DescribeMaterial} />
       </Stack.Navigator> 
     </NavigationContainer>
