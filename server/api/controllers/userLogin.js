757b8158930b65c66be3047ee5c0e5e26477490f
Andrew VanNus <avannus@gmail.com>
2023-03-06 16:32:53 -0500

    added authUser helper

--- server/api/controllers/userLogin.js
+++ server/api/controllers/userLogin.js
@@ -1,6 +1,4 @@
-import { model } from 'mongoose';
-import bcrypt from 'bcryptjs';
-const User = model('Users');
+import { authUser } from '../utils/authUser.js';
 
 /**
  * User login
@@ -10,27 +8,13 @@ const User = model('Users');
  */
 
 export function userLogin(req, res) {
-  if (!req.body.username || !req.body.password) {
-    res.status(400).send(
-      {
-        error: true,
-        message: 'Please provide username/password',
-      },
-    );
-    return;
-  }
-
-  User.findOne({ username: req.body.username }, (err, usr) => {
-    if (err) {
-      res.send(err);
+  authUser(req).then((usrAuth) => {
+    if (usrAuth.error) {
+      res.status(400).send(usrAuth);
       return;
     }
 
-    if (usr && bcrypt.compareSync(req.body.password, usr.password)) {
-      const { accountType, isVerified } = usr;
-      res.json({ accountType, isVerified });
-    } else {
-      res.status(401).send({ error: true, message: 'Invalid username/password' });
-    }
+    const { accountType, isVerified } = usrAuth.usr;
+    res.json({ accountType, isVerified });
   });
 }
---
+++ server/api/utils/authUser.js
@@ -0,0 +1,38 @@
+import { model } from 'mongoose';
+import bcrypt from 'bcryptjs';
+const User = model('Users');
+
+/**
+ * Helper to check if a user is logged in.
+ *
+ * @param {*} req http request data, the userID
+ * @param {*} res { accountType, isVerified }
+ */
+
+export function authUser(req) {
+  if (!req.body.username || !req.body.password) {
+    return {
+      error: 'Please provide username/password',
+    };
+  }
+
+  return User.findOne({ username: req.body.username }).then((usr, err) => {
+    if (err) {
+      console.log(`Error in authUser: ${err}`);
+      return {
+        error: 'User not found',
+      };
+    }
+
+    if (!(usr && bcrypt.compareSync(req.body.password, usr.password))) {
+      return {
+        error: 'Invalid username/password',
+      };
+    }
+
+    return {
+      error: false,
+      usr,
+    };
+  });
+}
