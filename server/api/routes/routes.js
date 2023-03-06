7f91256d81452ff116701856f501df41d29178fd
Andrew VanNus <avannus@gmail.com>
2023-03-06 17:54:24 -0500

    userExists, utils/getUser

--- server/api/routes/routes.js
+++ server/api/routes/routes.js
@@ -1,7 +1,9 @@
 import { userCreate } from '../controllers/userCreate.js';
 import { userLogin } from '../controllers/userLogin.js';
+import { userExists } from '../controllers/userExists.js';
 
 export default (app) => {
   app.route('/user/create').post(userCreate);
   app.route('/user/login').get(userLogin);
+  app.route('/user/exists').get(userExists);
 };
