4510224165d199fb1e8a0c14e732ab24eed4c4b1
May Everest <2mayeverest@gmail.com>
2023-03-13 11:11:09 -0400

    Go from Schedule Pickups -> Describe Materials

--- client/routes/SchedulePickups/index.js
+++ client/routes/SchedulePickups/index.js
@@ -3,38 +3,43 @@ import { View } from 'react-native';
 import RMText from '../../components/RMText';
 import RMStyle from '../../RMStyle';
 import RMButton from '../../components/RMButton';
-import Button from '../../components/RMButton';
+import PropTypes from 'prop-types';
 
-function SchedulePickups() {
+function SchedulePickups({ navigation }) {
   return (
     <View
       style={{
         flex: 1,
         alignItems: 'center',
-        //justifyContent: 'center',
+        // justifyContent: 'center',
         backgroundColor: RMStyle.colors.background,
       }}
     >
       <RMText>Schedule a Pickup</RMText>
-      <RMText>Select how often you would like your recyclables to be picked up:</RMText>
-      
-      <select name="pickup-frequency" id="pickup-frequency">
-        <option value="weekly">Weekly</option>
-        <option value="biweekly">Biweekly</option>
-        <option value="monthly">Monthly</option>
-        <option value="bimonthly">Bimonthly</option>
+      <RMText>
+        Select how often you would like your recyclables to be picked up:
+      </RMText>
+
+      <select name='pickup-frequency' id='pickup-frequency'>
+        <option value='weekly'>Weekly</option>
+        <option value='biweekly'>Biweekly</option>
+        <option value='monthly'>Monthly</option>
+        <option value='bimonthly'>Bimonthly</option>
       </select>
 
       <RMButton
         theme='primary'
         label='Confirm'
         onPress={() => {
-          navigation.navigate('PickupConfirmation');
+          navigation.navigate('DescribeMaterial');
         }}
       />
-
     </View>
   );
 }
+
+SchedulePickups.propTypes = {
+  navigation: PropTypes.object.isRequired,
+};
 
 export default SchedulePickups;
