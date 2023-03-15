85b63db6b1349b200ad15b55f85f6e19ec0ca7ae
May Everest <2mayeverest@gmail.com>
2023-03-15 18:08:51 -0400

    First attempt to fix error

--- client/routes/SchedulePickups/index.js
+++ client/routes/SchedulePickups/index.js
@@ -4,8 +4,18 @@ import RMText from '../../components/RMText';
 import RMStyle from '../../RMStyle';
 import RMButton from '../../components/RMButton';
 import PropTypes from 'prop-types';
+import DropDownPicker from 'react-native-dropdown-picker';
 
 function SchedulePickups({ navigation }) {
+  const [open, setOpen] = React.useState(false);
+  const [value, setValue] = React.useState(null);
+  const [items, setItems] = React.useState([
+    { label: 'Weekly', value: 'weekly' },
+    { label: 'Biweekly', value: 'biweekly' },
+    { label: 'Monthly', value: 'monthly' },
+    { label: 'Bimonthly', value: 'bimonthly' },
+  ]);
+  const [visible, setVisible] = React.useState(true);
   return (
     <View
       style={{
@@ -20,20 +30,32 @@ function SchedulePickups({ navigation }) {
         Select how often you would like your recyclables to be picked up:
       </RMText>
 
-      <select name='pickup-frequency' id='pickup-frequency'>
-        <option value='weekly'>Weekly</option>
-        <option value='biweekly'>Biweekly</option>
-        <option value='monthly'>Monthly</option>
-        <option value='bimonthly'>Bimonthly</option>
-      </select>
+      <View style={{ width: '80%', paddingBottom: 10 }}>
+        <DropDownPicker
+          open={open}
+          value={value}
+          items={items}
+          setOpen={setOpen}
+          setValue={setValue}
+          setItems={setItems}
+          onOpen={() => {
+            setVisible(false);
+          }}
+          onClose={() => {
+            setVisible(true);
+          }}
+        />
+      </View>
 
-      <RMButton
-        theme='primary'
-        label='Confirm'
-        onPress={() => {
-          navigation.navigate('DescribeMaterial');
-        }}
-      />
+      {visible && (
+        <RMButton
+          theme='primary'
+          label='Confirm'
+          onPress={() => {
+            navigation.navigate('DescribeMaterial');
+          }}
+        />
+      )}
     </View>
   );
 }
